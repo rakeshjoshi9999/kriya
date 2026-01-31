edf1601c961f910e1eaa67154f9ac6681d309e4d
Rakesh Joshi <rakeshjoshi9999@gmail.com>
2026-01-31 06:53:31 +0530

    Initial Project Setup with Next.js, Tailwind and shadcn

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+const page = () => {
+  return <div className="font-semibold">Welcome to the Home Page
+  <Button variant="ghost">
+    Click</Button></div>;
+}
+
+export default page;
